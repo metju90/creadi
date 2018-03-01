8767aff1bc45959126e1ae0a96c34e410db07162
Metju90 <barbaramatthew90@gmail.com>
2018-03-01 10:53:01 +0100

    removed used code

--- src/components/InsuranceForm/Form.js
+++ src/components/InsuranceForm/Form.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { capitalizeFirstLetter } from '../../utils';
 import { reduxForm } from 'redux-form';
 import * as skin from './skin';
 
@@ -8,7 +7,6 @@ const StyledForm = styled.form`${skin.Form}`;
 let Form = props => <StyledForm {...props} />;
 
 Form = reduxForm({
-  // a unique name for the form
   form: 'newInsurance',
 })(Form);
 
