6dfab4956fee7c1455fe88513cffbf7265a579e0
Matthew <barbaramatthew90@gmail.com>
2018-07-04 22:47:08 +0200

    further TS work

--- src/components/BrandLogo/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-import * as skin from './skin';
-
-const Text = styled.div`${skin.Text}`;
-
-const BrandLogo = () => <Text>#Mysurance</Text>;
-
-export default BrandLogo;
